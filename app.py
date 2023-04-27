b72e73b4a211aa7d697dc56e275e5e7e821c523d
Maximiliano Millan <mmillan@provinciamicrocreditos.com>
2023-04-27 16:12:57 -0300

    mend

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-from app.jiraModule.components.getAllProjects.view_getAllProjects import getAllProjects_bp
-from app.jiraModule.components.test.test import test_bp
-from app.jiraModule.components.createIssue.view_createIssue import createIssue_bp
-from app.jiraModule.components.getIssues.getIssues import getIssues_bp
-from app.jiraModule.components.getIssueForID.getIssueForID import getIssueForID_bp
-from app.jiraModule.components.home.home import home_bp
+from source.jiraModule.components.getAllProjects.view_getAllProjects import getAllProjects_bp
+from source.jiraModule.components.test.test import test_bp
+from source.jiraModule.components.createIssue.view_createIssue import createIssue_bp
+from source.jiraModule.components.getIssues.getIssues import getIssues_bp
+from source.jiraModule.components.getIssueForID.getIssueForID import getIssueForID_bp
+from source.jiraModule.components.home.home import home_bp
 
 
 
